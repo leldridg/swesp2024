805c60986cc3fd40d40c464421d8fe0742a9329e
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-02-28 11:38:03 -0600

    Home Page app.use + Formatted files

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 
+const home = require("./routes/home.js");
 const login = require("./routes/login.js");
+
 const app = express();
 
 const bodyParser = require('body-parser')
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const port = 3000;
 
 const path = require('path');
- 
+
 // Static Middleware
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -17,17 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // console.log(app.use(express.static(path.join(__dirname, 'public'))))
 
 
-app.get("/", function (req, res) {
-	// res.render(`pages/home`);
-	res.render('pages/home', { title: 'Home Page', message: 'Welcome to my INDEX Express app!'});
-
-});
-
-// app.get("/login", function (req, res) {
-
-// 	res.render('pages/login', {});
-// });
-
+app.use("/", home);
 app.use("/login", login);
 
 
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,9 @@
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+  res.render('pages/home', { title: 'Home Page', message: 'Welcome to my INDEX Express app!' });
 });
 
 module.exports = router;
