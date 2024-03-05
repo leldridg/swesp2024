78e6e95a43b6f692db86cc7d03b3dc7d069d40b8
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-03-05 11:57:07 -0600

    full

--- routes/login.js
+++ routes/login.js
@@ -12,46 +12,36 @@ router.post('/', function(req, res) {
   const { username, password } = req.body; // Extracting username and password from the form submission
 
   queries.loginValid(username, password, (err, valid) => {
+    if(err) { console.log(err); }
 
-    if(err) {
-      console.log(err);
+    if(valid){
+
+      queries.idByUsername(username, (err,result) => {
+        if(err) { console.log(err); }
+
+        console.log(result.user_id);
+
+        const generateRandomAlphanumeric = length => Array.from({length}, () => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".charAt(Math.floor(Math.random() * 62))).join('');
+
+          let sess = generateRandomAlphanumeric(20);
+          console.log(sess);
+
+          queries.removeToken(username)
+          queries.checkTokenUnique(sess, (err,valid) =>{
+            if(err) { console.log(err); }
+            console.log(valid);
+            if(valid){
+              res.send('Account login success');
+            } else {
+              console.log('non unique token created');
+            }
+        });
+      });
+
+    } else { // ACCOUNT LOGIN INCORRECT
+      res.send('Account login failed');
     }
-
-    queries.idByUsername(username, (err,user_id) => {
-      if(err) {
-        console.log(err);
-      }
-
-      console.log()
-    });
-
-    // if(valid){
-    //   const generateRandomAlphanumeric = length => Array.from({length}, () => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".charAt(Math.floor(Math.random() * 62))).join('');
-
-    //     let sess = generateRandomAlphanumeric(20);
-    //     console.log(sess);
-
-    //     queries.removeToken(username)
-    //     queries.checkTokenUnique(sess, (err,valid) =>{
-
-    //       if(err) {
-    //         console.log(err);
-    //       }
-      
-    //       console.log(valid);
-    //       if(valid){
-    //         res.send('Account login success');
-    //       } else {
-    //         console.log('non unique token created');
-    //       }
-    //     });
-
-
-    // } else {
-    //   res.send('Account login failed');
-    // }
-  });
-    
+  });  
 });
 
 module.exports = router;
