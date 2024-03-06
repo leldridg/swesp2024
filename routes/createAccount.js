c9d3398e931ad1b73be8821c5dd27f17f8cd060d
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-03-06 12:58:53 -0600

    create account middleware error handlinlg

--- routes/createAccount.js
+++ routes/createAccount.js
@@ -6,34 +6,38 @@ const queries = require('../database/queries');
 router.get('/', function (req, res, next) {
   res.render('pages/create-account', { taken: false } );
 });
-
-router.post('/', function(req, res) {
+router.post('/', function(req, res, next) { // Include 'next' in the function parameters
 
   const { username, password } = req.body; // Extracting username and password from the form submission
 
   // query for if username is already taken
   queries.accountTaken(username, (err, taken) => {
-    if(err) { console.log(err); }
+    if (err) { return next(err); }
 
-    if(taken){  
-      // res.send('Username already taken');
+    if (taken) {  
+      // Username already taken, render the page with a flag and the submitted password
       res.render('pages/create-account', {taken: true, password: password});
     } else {
       
-      // query to add a new user to the database
+      // Query to add a new user to the database
       queries.createAccount(username, password, (err, success) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send('An error occurred');
-        }
+        if (err) { return next(err); }
+        // If the account is successfully created, send a success message
         res.send('Account created successfully');
+        // will need to create a session token and login and such
       });
     }
-
   });
-
-
 });
+
+// Error-handling middleware
+router.use((err, req, res, next) => {
+  console.error(err); // Log the error information for debugging purposes
+  const statusCode = err.statusCode || 500;
+  const message = statusCode === 500 ? 'Something went wrong on the server.' : err.message;
+  res.status(statusCode).json({ error: message });
+});
+
 
 
 module.exports = router;
