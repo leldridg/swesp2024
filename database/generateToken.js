08cbc48391e89254f69b6456ad283e4aa25eb1bc
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2024-03-06 13:20:46 -0600

    used callbacks for generation of token now

--- database/generateToken.js
+++ database/generateToken.js
@@ -3,7 +3,7 @@ const queries = require('../database/queries');
 function generateToken(username, callback) {
 
   queries.idByUsername(username, (err, result) => {
-    if (err) { console.log(err); }
+    if (err) { return callback(err, null)}
 
     const generateRandomAlphanumeric = length => Array.from({ length }, () => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".charAt(Math.floor(Math.random() * 62))).join('');
 
@@ -11,12 +11,12 @@ function generateToken(username, callback) {
 
     // remove the token if it exists
     queries.removeToken(result.user_id, (err) => {
-      if (err) { console.log(err); }
+      if (err) { return callback(err, null)}
     });
 
+    // it should really never be unique.
     queries.checkTokenUnique(sess, (err, valid) => {
-      if (err) { console.log(err); }
-
+      if (err) { return callback(err, null)}
       if (valid) {
 
         queries.insertToken(sess, result.user_id, (err) => {
