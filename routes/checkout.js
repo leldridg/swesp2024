d497bae13915d3c2f96956c0469c33ccc5ae0ca2
Leanna Eldridge <leann@mbp-leldridg.trinity.local>
2024-03-20 22:13:08 -0500

    Tested functionality of new query and function by using them to implement admin redirect off of checkout page.

--- routes/checkout.js
+++ routes/checkout.js
@@ -2,8 +2,21 @@
 var express = require('express');
 var router = express.Router();
 
+const func = require('../database/isTokenAdmin');
+
 router.get('/', function (req, res, next) {
-  res.render('pages/checkout');
+  if (Object.keys(req.query).length != 0) {
+    func.isTokenAdmin(req.query.session, (err, exists, is_admin) => {
+      if (err) { return next(err); }
+      if (exists && is_admin) {
+        res.redirect(`/?session=${req.query.session}`);
+      } else {
+        res.render('pages/checkout');
+      }
+    });
+  } else {
+    res.render('pages/checkout');
+  }
 });
 
 module.exports = router;
