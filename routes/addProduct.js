1e45b570ba359fc38683ffdea42006d66faae5d2
Leanna Eldridge <leann@mbp-leldridg.trinity.local>
2024-03-26 09:59:05 -0500

    making progress on addProduct redirect

--- routes/addProduct.js
+++ routes/addProduct.js
@@ -2,8 +2,22 @@
 var express = require('express');
 var router = express.Router();
 
+const func = require('../database/isTokenAdmin');
+
+//test!!
 router.get('/', function (req, res, next) {
-  res.render('pages/add-product');
+  if (Object.keys(req.query).length != 0) {
+    func.isTokenAdmin(req.query.session, (err, exists, is_admin) => {
+      if (err) { return next(err); }
+      if (exists && is_admin) {
+        res.render('pages/add-product');
+      } else {
+        res.redirect(`/?session=${req.query.session}`);
+      }
+    });
+  } else {
+    res.redirect('pages/home');
+  }
 });
 
 module.exports = router;
