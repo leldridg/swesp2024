e06d93e921b45e9c142cc164ecc9213b97f1b8f1
leldridg <99363094+leldridg@users.noreply.github.com>
2024-03-28 14:50:21 -0500

    Revert "Testing"

--- routes/home.js
+++ routes/home.js
@@ -6,11 +6,30 @@ const queries = require('../database/queries');
 
 const userCart = require('../database/userCart');
 
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
 
   console.log(req.query);
 
-  res.render('pages/home');
+  if(Object.keys(req.query).length === 0){
+    res.render('pages/home', { title: 'welcome, guest user!', items: null});
+  } else {
+    queries.uidFromSID(req.query.session, (err, exists, user_id) => {
+      if (err) { return next(err); }
+
+      if(exists){
+        // gets username and cart items
+        queries.usernameByUID(user_id, (err,username) => {
+          userCart.getUserCart(user_id, (err, items) => {
+
+            res.render('pages/home', { title: `welcome ${username}`, items: items});
+          });
+        });
+      } else {
+        console.log("invalid session token");
+        res.send("invalid session token ):");
+      }
+    });
+  }
 
 });
 
