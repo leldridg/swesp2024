f7013207326354c26eb5c38c4b575d754d1b3d6c
twest411 <twest1@trinity.edu>
2024-03-28 14:56:36 -0500

    fixed home.js

--- routes/home.js
+++ routes/home.js
@@ -1,25 +1,19 @@
 
 var express = require('express');
 var router = express.Router();
-
 const queries = require('../database/queries');
-
-const userCart = require('../database/userCart');
 
 router.get('/', function (req, res, next) {
 
-  console.log(req.query);
-
-  res.render('pages/home');
+  queries.fetchProducts((err, items) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('An error occurred');
+    }
+    res.render('pages/home', { title: "Home!", items: items });
+  });
 
 });
 
-// Error-handling middleware
-router.use((err, req, res, next) => {
-  console.error(err); // Log the error information for debugging purposes
-  const statusCode = err.statusCode || 500;
-  const message = statusCode === 500 ? 'Something went wrong on the server.' : err.message;
-  res.status(statusCode).json({ error: message });
-});
 
 module.exports = router;
