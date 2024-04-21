22a9e27421677d30b59206a46d6157931a0fcdb6
Leanna Eldridge <leldridg@trinity.edu>
2024-04-21 14:08:54 -0500

    trying to make it so production db is not affected

--- test/frontend.test.js
+++ test/frontend.test.js
@@ -1,10 +1,18 @@
 const axios = require('axios');
-//const db = require('../database/db.js');
-//const queries = require('../database/queries.js');
+const db = require('../database/db.js');
+const queries = require('../database/queries.js');
 const generator = require('../database/generateToken.js');
 
 //having issues with db.query('BEGIN'); and db.query('ROLLBACK');
+
+beforeEach(async () => {
+    await db.query('BEGIN;');
+});
   
+afterEach(async () => {
+    await db.query('ROLLBACK;');
+});
+
 //test that visiting each page without a token is handles gracefully
 
 describe('test that visiting each page without a token is handled gracefully', () => {
@@ -95,7 +103,7 @@ describe('test that visiting each page without a token is handled gracefully', (
     });
 
     test('check server response code for edit product page', async () => {
-        const url = 'http://localhost:3000/edit-product'; // Replace with your URL
+        const url = 'http://localhost:3000/edit-product/1'; // Replace with your URL
         try {
             const response = await axios.get(url);
             console.log('Response Status Code:', response.status);
@@ -115,12 +123,18 @@ describe('test that visiting each page with an admin token is handled gracefully
 
     test('check server response code for home page', async () => {
 
-        let session_id = '';
+        const session_id = 'testadminsession';
 
-        generator.generateToken('admin', (err, token) => {
+        queries.insertToken(session_id, '1', (err) => {
             if (err) { console.error(err); }
-            session_id = token;
         });
+
+        // let session_id = '';
+
+        // generator.generateToken('admin', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
 
         const url = `http://localhost:3000/?session=${session_id}`; // Replace with your URL
         
@@ -137,12 +151,18 @@ describe('test that visiting each page with an admin token is handled gracefully
 
     test('check server response code for create account page', async () => {
 
-        let session_id = '';
+        const session_id = 'testadminsession';
 
-        generator.generateToken('admin', (err, token) => {
+        queries.insertToken(session_id, '1', (err) => {
             if (err) { console.error(err); }
-            session_id = token;
         });
+
+        // let session_id = '';
+
+        // generator.generateToken('admin', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
 
         const url = `http://localhost:3000/create-account/?session=${session_id}`; // Replace with your URL
         try {
@@ -157,12 +177,18 @@ describe('test that visiting each page with an admin token is handled gracefully
 
     test('check server response code for login page', async () => {
 
-        let session_id = '';
+        const session_id = 'testadminsession';
 
-        generator.generateToken('admin', (err, token) => {
+        queries.insertToken(session_id, '1', (err) => {
             if (err) { console.error(err); }
-            session_id = token;
         });
+
+        // let session_id = '';
+
+        // generator.generateToken('admin', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
 
         const url = `http://localhost:3000/login/?session=${session_id}`; // Replace with your URL
         try {
@@ -177,12 +203,18 @@ describe('test that visiting each page with an admin token is handled gracefully
 
     test('check server response code for cart page', async () => {
 
-        let session_id = '';
+        const session_id = 'testadminsession';
 
-        generator.generateToken('admin', (err, token) => {
+        queries.insertToken(session_id, '1', (err) => {
             if (err) { console.error(err); }
-            session_id = token;
         });
+
+        // let session_id = '';
+
+        // generator.generateToken('admin', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
 
         const url = `http://localhost:3000/cart/?session=${session_id}`; // Replace with your URL
         try {
@@ -197,12 +229,18 @@ describe('test that visiting each page with an admin token is handled gracefully
 
     test('check server response code for view product page', async () => {
 
-        let session_id = '';
+        const session_id = 'testadminsession';
 
-        generator.generateToken('admin', (err, token) => {
+        queries.insertToken(session_id, '1', (err) => {
             if (err) { console.error(err); }
-            session_id = token;
         });
+
+        // let session_id = '';
+
+        // generator.generateToken('admin', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
 
         const url = `http://localhost:3000/view-product/1/?session=${session_id}`; // Replace with your URL
         try {
@@ -217,12 +255,18 @@ describe('test that visiting each page with an admin token is handled gracefully
 
     test('check server response code for checkout page', async () => {
 
-        let session_id = '';
+        const session_id = 'testadminsession';
 
-        generator.generateToken('admin', (err, token) => {
+        queries.insertToken(session_id, '1', (err) => {
             if (err) { console.error(err); }
-            session_id = token;
         });
+
+        // let session_id = '';
+
+        // generator.generateToken('admin', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
 
         const url = `http://localhost:3000/checkout/?session=${session_id}`; // Replace with your URL
         try {
@@ -237,12 +281,18 @@ describe('test that visiting each page with an admin token is handled gracefully
 
     test('check server response code for add product page', async () => {
 
-        let session_id = '';
+        const session_id = 'testadminsession';
 
-        generator.generateToken('admin', (err, token) => {
+        queries.insertToken(session_id, '1', (err) => {
             if (err) { console.error(err); }
-            session_id = token;
         });
+
+        // let session_id = '';
+
+        // generator.generateToken('admin', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
 
         const url = `http://localhost:3000/add-product/?session=${session_id}`; // Replace with your URL
         try {
@@ -257,14 +307,19 @@ describe('test that visiting each page with an admin token is handled gracefully
 
     test('check server response code for edit product page', async () => {
 
-        let session_id = '';
+        const session_id = 'testadminsession';
 
-        generator.generateToken('admin', (err, token) => {
+        queries.insertToken(session_id, '1', (err) => {
             if (err) { console.error(err); }
-            session_id = token;
         });
+        // let session_id = '';
 
-        const url = `http://localhost:3000/edit-product/?session=${session_id}`; // Replace with your URL
+        // generator.generateToken('admin', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/edit-product/1/?session=${session_id}`; // Replace with your URL
         try {
             const response = await axios.get(url);
             console.log('Response Status Code:', response.status);
@@ -277,9 +332,220 @@ describe('test that visiting each page with an admin token is handled gracefully
 
 });
 
-
 //test that handling each page with a non-admin token is handled gracefully
 
+describe('test that visiting each page with a non-admin token is handled gracefully', () => {
 
+    test('check server response code for home page', async () => {
+
+        const session_id = 'testnonadminsession';
+
+        queries.insertToken(session_id, '3', (err) => {
+            if (err) { console.error(err); }
+        });
+
+        // let session_id = '';
+
+        // generator.generateToken('petlover123', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/?session=${session_id}`; // Replace with your URL
+        
+        try {
+            const response = await axios.get(url);
+            console.log('Response Status Code:', response.status);
+            expect(response.status).toBe(200); // Change the expected status code as needed
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+
+    });
+
+    test('check server response code for create account page', async () => {
+
+        const session_id = 'testnonadminsession';
+
+        queries.insertToken(session_id, '3', (err) => {
+            if (err) { console.error(err); }
+        });
+
+        // let session_id = '';
+
+        // generator.generateToken('petlover123', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/create-account/?session=${session_id}`; // Replace with your URL
+        try {
+            const response = await axios.get(url);
+            console.log('Response Status Code:', response.status);
+            expect(response.status).toBe(200); // Change the expected status code as needed
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+    });
+
+    test('check server response code for login page', async () => {
+
+        const session_id = 'testnonadminsession';
+
+        queries.insertToken(session_id, '3', (err) => {
+            if (err) { console.error(err); }
+        });
+
+        // let session_id = '';
+
+        // generator.generateToken('petlover123', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/login/?session=${session_id}`; // Replace with your URL
+        try {
+            const response = await axios.get(url);
+            console.log('Response Status Code:', response.status);
+            expect(response.status).toBe(200); // Change the expected status code as needed
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+    });
+
+    test('check server response code for cart page', async () => {
+
+        const session_id = 'testnonadminsession';
+
+        queries.insertToken(session_id, '3', (err) => {
+            if (err) { console.error(err); }
+        });
+
+        // let session_id = '';
+
+        // generator.generateToken('petlover123', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/cart/?session=${session_id}`; // Replace with your URL
+        try {
+            const response = await axios.get(url);
+            console.log('Response Status Code:', response.status);
+            expect(response.status).toBe(200); // Change the expected status code as needed
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+    });
+
+    test('check server response code for view product page', async () => {
+
+        const session_id = 'testnonadminsession';
+
+        queries.insertToken(session_id, '3', (err) => {
+            if (err) { console.error(err); }
+        });
+
+        // let session_id = '';
+
+        // generator.generateToken('petlover123', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/view-product/1/?session=${session_id}`; // Replace with your URL
+        try {
+            const response = await axios.get(url);
+            console.log('Response Status Code:', response.status);
+            expect(response.status).toBe(200); // Change the expected status code as needed
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+    });
+
+    test('check server response code for checkout page', async () => {
+
+        const session_id = 'testnonadminsession';
+
+        queries.insertToken(session_id, '3', (err) => {
+            if (err) { console.error(err); }
+        });
+
+        // let session_id = '';
+
+        // generator.generateToken('petlover123', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/checkout/?session=${session_id}`; // Replace with your URL
+        try {
+            const response = await axios.get(url);
+            console.log('Response Status Code:', response.status);
+            expect(response.status).toBe(200); // Change the expected status code as needed
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+    });
+
+    test('check server response code for add product page', async () => {
+
+        const session_id = 'testnonadminsession';
+
+        queries.insertToken(session_id, '3', (err) => {
+            if (err) { console.error(err); }
+        });
+
+        // let session_id = '';
+
+        // generator.generateToken('petlover123', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/add-product/?session=${session_id}`; // Replace with your URL
+        try {
+            const response = await axios.get(url);
+            console.log('Response Status Code:', response.status);
+            expect(response.status).toBe(200); // Change the expected status code as needed
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+    });
+
+    test('check server response code for edit product page', async () => {
+
+        const session_id = 'testnonadminsession';
+
+        queries.insertToken(session_id, '3', (err) => {
+            if (err) { console.error(err); }
+        });
+
+        // let session_id = '';
+
+        // generator.generateToken('petlover123', (err, token) => {
+        //     if (err) { console.error(err); }
+        //     session_id = token;
+        // });
+
+        const url = `http://localhost:3000/edit-product/1/?session=${session_id}`; // Replace with your URL
+        try {
+            const response = await axios.get(url);
+            console.log('Response Status Code:', response.status);
+            expect(response.status).toBe(200); // Change the expected status code as needed
+        } catch (error) {
+            console.error('Error:', error.message);
+            throw error;
+        }
+    });
+
+});
 
 
